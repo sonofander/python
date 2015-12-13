dbb3be52bac32582d8bde8ef676477e94ab831ed
Cameron <cameron.p.buckingham@gmail.com>
2015-12-13 06:33:46 -0700

    scope

---
+++ scope.py
@@ -0,0 +1,13 @@
+a = 2345
+
+def corn():
+    #a = 2345
+    print(a)
+
+def peas():
+    print(a)
+
+corn()
+peas()
+
+# whenever a variable is created inside a function, only that function can use it
