293b78d768b8d35126973a3e7f5eb34c098e4963
Cameron <cameron.p.buckingham@gmail.com>
2015-12-27 15:57:57 -0500

    init

---
+++ init.py
@@ -0,0 +1,28 @@
+class tuna:
+
+    def __init__(self):
+        #instanciate
+        print(blr)
+
+    def swim(self):
+        print("I am swimming")
+
+
+    flipper = tuna()
+    flipper.swim()
+     ## calls init first :)
+
+class Enemy
+    def __init__(self, x):
+        self.energy = x
+
+        def get_energy(self):
+            print(self.energy)
+
+            jason = Enemy(5)
+            sandy = Enemy(18)
+
+            jason.get_energy()
+            sandy.get_energy
+
+             # init populates info for all classes that can be passed at creation
