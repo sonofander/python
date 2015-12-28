aeae1935ecf96c3ded0caf92d271033a5792cda8
Cameron <cameron.p.buckingham@gmail.com>
2015-12-28 11:31:10 -0500

    if else

---
+++ if_else.py
@@ -0,0 +1,16 @@
+age = 27
+
+if age > 21
+print "You can buy beer"
+else
+print "You are too young to buy beer"
+
+name = "cam"
+if name is "buck":
+    print ("what up buck")
+elif name is "cam":
+    print ("what up cam")
+elif name is "sammy":
+    print ("what up sammy")
+else:
+    print("please sign up for the site")
