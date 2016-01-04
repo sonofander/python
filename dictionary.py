54666c39d62eb3296d7d68618139c697a9640f40
Cameron <cameron.p.buckingham@gmail.com>
2016-01-04 08:27:38 -0700

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+#keys and values
+#dictionary is a hash
+#keys and values
+classmates = {'tony':"cool but smells", 'david':'fat and old', 'emma':'sits behind me'}
+
+print (classmates)
+print (classmates[emma])
+#.items is important or key value iterating
+ for k, v in classmates.items():
+     print (k + v)
